ad99c5be222ad3d4c5ba7363b223addb2718326f
max.s <max.s@nsysgroup.com>
2023-05-21 14:27:11 +0300

    Добавлен метод отправки данных

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 import queries
 
@@ -9,6 +9,12 @@ app = Flask(__name__)
 def main_page():
     directions = queries.direction_list()
     return render_template('main.html', directions=directions)
+
+
+@app.route('/getdata', methods=['POST'])
+def getdata():
+    print(request.get_json())
+    return jsonify({'data': 'OK'})
 
 
 if __name__ == "__main__":
